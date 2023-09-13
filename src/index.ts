941e1eb8e25668aed8ce7777b56256fbb34421e4
monte-dev <119732972+monte-dev@users.noreply.github.com>
2023-09-13 18:18:05 +0000

    added class message for handling the messages

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,68 @@
 const inquirer = require('inquirer');
+const consola = require('consola')
 
-inquirer
-	.prompt([
-		{
-			name: 'name',
-			type: 'input',
-			message: "What's your name?",
-		},
-		{
-			name: 'age',
-			type: 'number',
-			message: 'How old are you?',
-			default: 18,
-		},
-	])
-	.then((answers: { name: string; age: number }) => {
-		console.log(`\nHi ${answers.name}. ${answers.age}? Nice! \n`);
-	});
+enum Action {
+	List = "list",
+	Add = "add",
+	Remove = "remove",
+	Quit = "quit"
+  }
+ 
+enum Variant {
+  Success = "success",
+  Error = "error",
+  Info = "info"
+}
+
+  type InquirerAnswers = {
+	action: Action
+  }
+
+class Message  {
+  constructor(private content: string) {
+
+  }
+  show() {
+    console.log(this.content)
+  }
+ 
+  
+  capitalize() {
+    this.content = this.content.charAt(0).toUpperCase() + this.content.slice(1);
+  }
+  
+  toUpperCase() {
+    this.content = this.content.toUpperCase();
+  }
+  
+  toLowerCase() {
+    this.content = this.content.toLowerCase();
+  }
+  
+  static showColorized(Variant:string, text:string) {
+    if (Variant === 'success') {
+      consola.success(text);
+    } else if (Variant === 'error') {
+      consola.error(text);
+    } else if (Variant === 'info') {
+      consola.info(text);
+    }
+  }
+  }
+
+
+
+const startApp = () => {
+  inquirer.prompt([{
+    name: 'action',
+    type: 'input',
+    message: 'How can I help you?',
+  }]).then(async (answers: InquirerAnswers) => {
+    console.log("Chosen action: " + answers.action);
+    startApp();
+    if (answers.action === "quit")
+      return;
+  });
+}
+
+startApp();
