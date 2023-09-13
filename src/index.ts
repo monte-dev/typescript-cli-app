5f9373e4b5f785e7e5a9f025e897031f19fe0a2e
monte-dev <119732972+monte-dev@users.noreply.github.com>
2023-09-13 18:52:35 +0000

    add actions add, remove, quit

--- src/index.ts
+++ src/index.ts
@@ -49,8 +49,57 @@ class Message  {
     }
   }
   }
+  
+  //Na końcu dodaj metodę remove. Powinna ona przyjmować nazwę użytkownika w formie argumentu (stringa). Następnie powinna poszukać go w tablicy data. Jeśli uda się go znaleźć, to należy go usunać oraz wyświetlić komunikat sukcesu – User deleted!. Jeśli nie, to należy pokazać komunikat błędu – User not found.... Zadbaj o odpowiednie otypowanie samej funkcji, jak i o to, aby była dostępna publicznie.
 
+interface User {
+  name: string;
+  age: number;
+}
 
+class UsersData {
+  data: User[] = [];
+
+  showAll() {
+    if (this.data.length === 0) {
+      console.log("No data...");
+    } else {
+      Message.showColorized(Variant.Info, "Users data");
+      console.table(this.data);
+    }
+  }
+
+  add(user: User): void {
+    if (typeof user.age === 'number' && user.age > 0 && typeof user.name === 'string' && user.name.length > 0) {
+      this.data.push(user);
+      Message.showColorized(Variant.Success, "User has been successfully added!");
+    } else {
+      Message.showColorized(Variant.Error, "Wrong data!");
+    }
+  }
+
+  remove(name: string): void {
+    if(this.data.find(user=> user.name === name)) {
+      this.data = this.data.filter((user) => user.name !== name);
+      Message.showColorized(Variant.Success, 'User deleted')
+    } else {
+      Message.showColorized(Variant.Error, 'User not found...')
+    }
+  }
+}
+
+const users = new UsersData();
+
+console.log("\n");
+console.info("???? Welcome to the UsersApp!");
+console.log("====================================");
+Message.showColorized(Variant.Info, "Available actions");
+console.log("\n");
+console.log("list – show all users");
+console.log("add – add new user to the list");
+console.log("remove – remove user from the list");
+console.log("quit – quit the app");
+console.log("\n");
 
 const startApp = () => {
   inquirer.prompt([{
@@ -58,11 +107,38 @@ const startApp = () => {
     type: 'input',
     message: 'How can I help you?',
   }]).then(async (answers: InquirerAnswers) => {
-    console.log("Chosen action: " + answers.action);
+    switch (answers.action) {
+      case Action.List:
+        users.showAll();
+        break;
+      case Action.Add:
+        const user = await inquirer.prompt([{
+          name: 'name',
+          type: 'input',
+          message: 'Enter name',
+        }, {
+          name: 'age',
+          type: 'number',
+          message: 'Enter age',
+        }]);
+        users.add(user);
+        break;
+      case Action.Remove:
+        const name = await inquirer.prompt([{
+          name: 'name',
+          type: 'input',
+          message: 'Enter name',
+        }]);
+        users.remove(name.name);
+        break;
+      case Action.Quit:
+        Message.showColorized(Variant.Info, "Bye bye!");
+        return;
+        default:
+          Message.showColorized(Variant.Error, 'wrong command, please type one of \'list, add, remove, quit\' ')
+    }
+
     startApp();
-    if (answers.action === "quit")
-      return;
   });
 }
-
 startApp();
